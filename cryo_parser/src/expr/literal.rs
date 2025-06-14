88928501a4b97eb63d398385744dd3a2ad99e906
SunkenPotato <3.14radius02@gmail.com>
2025-06-14 22:59:35 +0200

    add literal parsing -- among others

--- cryo_parser/src/expr/literal.rs
+++ cryo_parser/src/expr/literal.rs
@@ -6,7 +6,17 @@
 //! - binding references
 //! - arithmetic expressions
 
-use crate::Parse;
+use crate::{Parse, ParseError};
+use cryo_lexer::literal::Literal as LToken;
+
+const ASCII_ZERO: u8 = 0x30;
+const NEGATIVE_SIGN: u8 = 0x2D;
+const SEPARATOR: u8 = 0x5F;
+
+const ESCAPE: char = '\\';
+const NEWLINE_ESCAPE: char = 'n';
+const TAB_ESCAPE: char = 't';
+const ZERO_ESCAPE: char = '0';
 
 /// A literal expression.
 ///
@@ -25,15 +35,165 @@ pub enum Literal {
 /// An integer literal.
 ///
 /// This literal has the same constraints as an [`i32`], since it is represented by an `i32`.
+#[derive(PartialEq, Eq, Debug, Clone, Copy, PartialOrd, Ord, Hash, Default)]
 pub struct NumberLiteral(pub i32);
 
 impl Parse for NumberLiteral {
     fn parse(stream: &mut crate::TokenStream) -> Result<Self, crate::ParseError> {
-        let token = stream.peek()
+        let (token, span) = stream.advance_require::<LToken>()?;
+        let LToken::NumberLiteral(num_lit) = token else {
+            return Err(ParseError::wrong_type(span, "number", "string"));
+        };
+
+        let mut integer = 0i32;
+        let mut sign = 1;
+
+        for c in num_lit.0.bytes() {
+            match c {
+                NEGATIVE_SIGN => sign *= -1,
+                SEPARATOR => continue,
+                // this is ok because the lexer has already checked the validity of these strings
+                c => {
+                    let digit = (c - ASCII_ZERO) as i32;
+                    integer = integer
+                        .checked_mul(10)
+                        .ok_or(ParseError::integer_overflow(span))?
+                        .checked_add(digit)
+                        .ok_or(ParseError::integer_overflow(span))?;
+                }
+            }
+        }
+
+        Ok(Self(integer * sign))
     }
 }
 
 /// An escaped string literal.
 ///
 /// This literal has the same constraints as a [`String`], since it is represented by a `String`.
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
 pub struct StringLiteral(pub String);
+
+impl Parse for StringLiteral {
+    // TODO: add support for unicode escapes
+    fn parse(stream: &mut crate::TokenStream) -> Result<Self, ParseError> {
+        let (token, span) = stream.advance_require::<LToken>()?;
+        let LToken::StringLiteral(str_lit) = token else {
+            return Err(ParseError::wrong_type(span, "string", "number"));
+        };
+
+        let mut s = String::new();
+        let mut iter = str_lit.0.chars().peekable();
+
+        while let Some(c) = iter.next() {
+            if c == ESCAPE {
+                let Some(escaped) = iter.peek() else {
+                    return Err(ParseError::invalid_escape(span));
+                };
+
+                match *escaped {
+                    NEWLINE_ESCAPE => s.push('\n'),
+                    TAB_ESCAPE => s.push('\t'),
+                    ZERO_ESCAPE => s.push('\0'),
+                    _ => return Err(ParseError::invalid_escape(span)),
+                }
+
+                iter.next();
+                continue;
+            }
+
+            s.push(c);
+        }
+
+        return Ok(Self(s));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::VecDeque;
+
+    use cryo_lexer::{
+        literal::{Literal, NumberLiteral as NL, StringLiteral as SL},
+        tokens::{Token, TokenType},
+    };
+    use cryo_span::Span;
+
+    use crate::{
+        Parse, ParseError, TokenStream,
+        expr::literal::{NumberLiteral, StringLiteral},
+    };
+
+    #[test]
+    fn parse_int_lit() {
+        let input = "--123_456_789".to_owned();
+        let token = Token::new(
+            TokenType::Literal(Literal::NumberLiteral(NL(input))),
+            Span::EMPTY,
+        );
+        let mut ts = TokenStream::new(VecDeque::from([token]));
+
+        assert_eq!(NumberLiteral::parse(&mut ts), Ok(NumberLiteral(123456789)))
+    }
+
+    #[test]
+    fn do_not_parse_overflow() {
+        let input = "2147483648".to_owned(); // (i32::MAX + 1)
+        let token = Token::new(
+            TokenType::Literal(Literal::NumberLiteral(NL(input))),
+            Span::EMPTY,
+        );
+
+        let mut ts = TokenStream::new(VecDeque::from([token]));
+
+        assert_eq!(
+            NumberLiteral::parse(&mut ts),
+            Err(ParseError::integer_overflow(Span::EMPTY))
+        )
+    }
+
+    #[test]
+    fn parse_str_lit() {
+        str_test("hello, world", "hello, world")
+    }
+
+    #[test]
+    fn parse_str_lit_with_escapes() {
+        str_test("hello, world\\n\\t\\0", "hello, world\n\t\0");
+    }
+
+    #[test]
+    fn do_not_parse_invalid_escape() {
+        str_test_fail("hello, world\\z", ParseError::invalid_escape(Span::EMPTY));
+    }
+
+    #[test]
+    fn do_not_parse_missing_escape() {
+        str_test_fail("hello, world\\", ParseError::invalid_escape(Span::EMPTY));
+    }
+
+    fn str_test(ex: &str, re: &str) {
+        let token = Token::new(
+            TokenType::Literal(Literal::StringLiteral(SL(ex.to_owned()))),
+            Span::EMPTY,
+        );
+        let mut ts = TokenStream::new(VecDeque::from([token]));
+
+        assert_eq!(
+            StringLiteral::parse(&mut ts),
+            Ok(StringLiteral(re.to_owned()))
+        );
+        assert!(ts.0.is_empty());
+    }
+
+    fn str_test_fail(ex: &str, err: ParseError) {
+        let token = Token::new(
+            TokenType::Literal(Literal::StringLiteral(SL(ex.to_owned()))),
+            Span::EMPTY,
+        );
+        let mut ts = TokenStream::new(VecDeque::from([token]));
+
+        assert_eq!(StringLiteral::parse(&mut ts), Err(err));
+        assert!(ts.0.is_empty());
+    }
+}
--- cryo_parser/src/lib.rs
+++ cryo_parser/src/lib.rs
@@ -7,60 +7,144 @@
 
 #![warn(clippy::missing_errors_doc, clippy::missing_panics_doc)]
 #![deny(missing_docs)]
-#![forbid(unsafe_code)]
 
 pub mod expr;
 
-use cryo_lexer::tokens::Token;
+use cryo_lexer::tokens::{Token, TokenGroup};
 use cryo_span::Span;
 
-use std::fmt::{Debug, Display};
+use std::{
+    borrow::Cow,
+    collections::VecDeque,
+    fmt::{Debug, Display},
+};
 
-pub struct TokenStream(Vec<Token>);
+/// A newtype wrapper around a [`VecDeque`] for parse operations.
+pub struct TokenStream(VecDeque<Token>);
 
 impl TokenStream {
-    pub const fn new(t: Vec<Token>) -> Self {
+    /// Create a new stream from a [`VecDeque`].
+    pub const fn new(t: VecDeque<Token>) -> Self {
         Self(t)
     }
 
+    /// Peek into the stream, retrieving the first element as a reference if it exists.
     pub fn peek(&self) -> Option<&Token> {
-        self.0.first()
+        self.0.front()
     }
 
-    pub fn advance(&mut self) -> Option<Token> {
-        self.0.pop()
+    /// Advance the stream, returning the first element in the stream if it exists.
+    pub fn advance(&mut self) -> Result<Token, ParseError> {
+        self.0.pop_front().ok_or(ParseError::END_OF_INPUT)
     }
 
-    pub fn peek_require<T: 'static>(&self) -> Option<&T> {
+    /// Peek into the stream and check if the next token is of `T`, if some.
+    ///
+    /// Returns `None` if the token is not of `T` or doesn't exist.
+    ///
+    /// See also: [`peek`]
+    pub fn peek_require<T: 'static + TokenGroup>(&self) -> Option<&T> {
         self.peek().map(Token::require_ref).flatten()
     }
 
-    pub fn advance_require<T: 'static>(&mut self) -> Result<T, Option<Token>> {
-        let inter = self.advance();
+    /// Advance the `TokenStream` and require the next token to be of `T`.
+    ///
+    /// Returns `Ok(T)` if the token is of `T`. \
+    /// If the token is not present or has the incorrect type, the stream will not be advanced
+    /// and the function will return `Err(())`.
+    pub fn advance_require<T: 'static + TokenGroup>(&mut self) -> Result<(T, Span), ParseError> {
+        let reference = self.peek().ok_or(ParseError::END_OF_INPUT)?;
 
-        match inter {
-            Some(v) => v.require().map_err(|_| None),
-            None => return Err(None),
+        if !reference.is::<T>() {
+            return Err(ParseError::wrong_token::<T>(reference));
+        }
+
+        let span = reference.span;
+
+        // SAFETY: it has been checked if the stream contains the next elements
+        // and that the token type is `T`.
+        unsafe {
+            Ok((
+                self.advance()
+                    .unwrap_unchecked()
+                    .require::<T>()
+                    .unwrap_unchecked(),
+                span,
+            ))
         }
     }
 }
 
+/// Trait for parsing operations.
+///
+/// Every type implementing this should be able to define and create itself from a
+/// certain sequence of [`Token`]s.
 pub trait Parse: Sized {
+    /// Create an instance of `Self` from a certain number of tokens passed into this function.
     fn parse(stream: &mut TokenStream) -> Result<Self, ParseError>;
 }
 
+/// Represents a parse error.
+///
+/// Contains a span pointing to where the problematic tokens are in the input,
+/// an error message, and a code.
 #[derive(Debug)]
 pub struct ParseError<'a> {
     span: Span,
-    message: &'a str,
+    message: Cow<'a, str>,
     code: u32,
 }
 
+impl ParseError<'static> {
+    /// The parser received an unexpected end of input.
+    pub const END_OF_INPUT: Self = Self::new_slice(Span::EMPTY, "unexpected end of input", 0);
+
+    /// The parser received the wrong token.
+    pub fn wrong_token<T: TokenGroup>(recv: &Token) -> Self {
+        Self::new_owned(
+            recv.span,
+            format!(
+                "invalid token: expected '{}', got '{}'",
+                T::NAME,
+                recv.name()
+            ),
+            1,
+        )
+    }
+
+    /// An invalid type was received (e.g., string literal instead of number literal).
+    pub fn wrong_type(span: Span, ex: &'static str, re: &'static str) -> Self {
+        let message = format!("invalid type: expected '{}', got '{}'", ex, re);
+
+        Self::new_owned(span, message, 2)
+    }
+
+    /// Literal integer overflow.
+    pub const fn integer_overflow(span: Span) -> Self {
+        Self::new_slice(span, "integer overflow", 3)
+    }
+
+    /// An invalid character was escaped / there is no escape character.
+    pub const fn invalid_escape(span: Span) -> Self {
+        Self::new_slice(span, "invalid/no escape character", 4)
+    }
+}
+
 impl<'a> ParseError<'a> {
-    pub const fn new(span: Span, message: &'a str, code: u32) -> Self {
+    /// Create a [`ParseError`] with a string slice message.
+    pub const fn new_slice(span: Span, message: &'a str, code: u32) -> Self {
         Self {
             span,
-            message,
+            message: Cow::Borrowed(message),
+            code,
+        }
+    }
+
+    /// Create a [`ParseError`] with an owned string message.
+    pub fn new_owned(span: Span, message: String, code: u32) -> Self {
+        Self {
+            span,
+            message: Cow::Owned(message),
             code,
         }
     }
@@ -68,12 +152,20 @@ impl<'a> ParseError<'a> {
 
 impl<'a> Display for ParseError<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        writeln!(f, "error[E{:0<5}]: {}", self.code, self.message);
-        write!(f, "--> {}", self.span);
+        writeln!(f, "error[E{:0<5}]: {}", self.code, self.message)?;
+        write!(f, "--> {}", self.span)?;
 
         Ok(())
     }
 }
+
+impl PartialEq for ParseError<'_> {
+    fn eq(&self, other: &Self) -> bool {
+        (self.code == other.code) && (self.span == other.span)
+    }
+}
+
+impl Eq for ParseError<'_> {}
 
 /// Parses a list of tokens into an AST.
 pub struct Parser {
