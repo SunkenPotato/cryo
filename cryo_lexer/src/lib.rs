a654bb8a0ec37a06ea896b0aa6d83fe6aefceb2d
SunkenPotato <3.14radius02@gmail.com>
2025-06-26 07:25:02 +0200

    initialize lexer

--- cryo_lexer/src/lib.rs
+++ cryo_lexer/src/lib.rs
@@ -1,14 +1,35 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use cryo_span::Spanned;
+
+pub type Token = Spanned<TokenType>;
+
+macro_rules! atom {
+    (
+        $(#[$attr:meta])*
+        #($atom:expr)
+        $visibility:vis struct $identifier:ident;
+    ) => {
+        $visibility struct $identifier;
+
+        impl $crate::Lex for $identifier {
+            fn lex(s: &str) -> Result<(Self, &str), $crate::LexicalError> {
+                let rest = s.strip_prefix($atom).ok_or($crate::LexicalError::SequenceNotFound($atom))?;
+                Ok((Self, rest))
+            }
+        }
+    };
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+trait Lex: Sized {
+    fn lex(s: &str) -> Result<(Self, &str), LexicalError>;
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[derive(Debug, Clone)]
+pub enum TokenType {}
+
+pub enum LexicalError<'a> {
+    SequenceNotFound(&'a str),
+}
+
+pub fn lexer(input: &str) -> Result<Vec<Token>, LexicalError> {
+    todo!()
 }
