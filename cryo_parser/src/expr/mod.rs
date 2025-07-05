78e131b8431aa8440eb56cc0d0f39c2d901aa120
SunkenPotato <3.14radius02@gmail.com>
2025-07-05 21:08:59 +0200

    optimise expression parsing

--- cryo_parser/src/expr/mod.rs
+++ cryo_parser/src/expr/mod.rs
@@ -4,13 +4,48 @@ pub mod math_expr;
 mod tests;
 
 use cryo_parser_proc_macro::Parse;
+use cryo_span::Spanned;
 
-use crate::expr::{literal::Literal, math_expr::MathExpr};
+use crate::{
+    expr::{
+        literal::Literal,
+        math_expr::{MathExpr, Operator},
+    },
+    parser::Parse,
+};
 
-#[derive(Parse, Debug, PartialEq)]
+// derive(Parse) cannot be applied to this to prevent double ReducedExpr parsing
+#[derive(Debug, PartialEq)]
 pub enum Expr {
     MathExpr(Box<MathExpr>),
     ReducedExpr(ReducedExpr),
+}
+
+impl Parse for Expr {
+    type Output = Self;
+    fn parse(
+        tokens: &mut cryo_lexer::stream::TokenStreamGuard,
+    ) -> crate::parser::ParseResult<Self::Output> {
+        let lhs = tokens.with(ReducedExpr::parse)?;
+        if let Ok(Spanned {
+            t: op,
+            span: op_span,
+        }) = tokens.with(Operator::parse)
+        {
+            let (rhs, rhs_span) = tokens.with(Self::parse)?.tuple();
+            let span = lhs.span + op_span + rhs_span;
+            Ok(Spanned::new(
+                Self::MathExpr(Box::new(MathExpr {
+                    lhs: lhs.t,
+                    op,
+                    rhs,
+                })),
+                span,
+            ))
+        } else {
+            Ok(lhs.map(Self::ReducedExpr))
+        }
+    }
 }
 
 #[derive(Parse, Debug, PartialEq)]
